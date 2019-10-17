76d77a9fcde71bb17658374ea0a2e546c7b83753
SHINU RAJ <51168016+toshinuraj@users.noreply.github.com>
2019-10-17 12:52:42 -0400

    Return;

--- coding/lab1/src/lab1.cpp
+++ coding/lab1/src/lab1.cpp
@@ -1,7 +1,7 @@
 
 
 #include <iostream>
-using namespace std;;
+using namespace std;
 
 int main() {
 	int i;
@@ -9,5 +9,5 @@ int main() {
 	{
 	cout << "Hello World :-)" << endl;
 	}
-	return 0;
+	return 0;;
 }
